2f9844538b4812b9f261f23e165e40ee97ec7f86
Manish Sharma <manish.sharma@cimcondigital.com>
2022-12-30 10:44:40 +0530

    Initial Commit

---
+++ MasteringSwiftUI/MasteringSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringSwiftUIApp.swift
+//  MasteringSwiftUI
+//
+//  Created by Manish  Sharma on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MasteringSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
